787d713dde382ab50616b0378181e73cfaf9e92c
Phoenix Bishea <phoenixb@cs.utexas.edu>
2016-02-23 18:18:46 -0600

    Add basic GameManager files and remove BaseApp and TutorialApp

---
+++ GameManager.h
@@ -0,0 +1,41 @@
+#ifndef GameManager_h
+#define GameManager_h
+ 
+#include <OgreRoot.h>
+#include <OgreWindowEventUtilities.h>
+ 
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+ 
+class GameManager
+  : public Ogre::WindowEventListener,
+    public Ogre::FrameListener
+{
+public:
+  GameManager();
+  virtual ~GameManager();
+ 
+  bool go();
+ 
+private:
+  virtual bool frameRenderingQueued(const Ogre::FrameEvent& fe);
+ 
+  virtual void windowResized(Ogre::RenderWindow* rw);
+  virtual void windowClosed(Ogre::RenderWindow* rw);
+ 
+  Ogre::Root* mRoot;
+  Ogre::String mResourcesCfg;
+  Ogre::String mPluginsCfg;
+  Ogre::RenderWindow* mWindow;
+  Ogre::SceneManager* mSceneMgr;
+  Ogre::Camera* mCamera;
+ 
+  OIS::InputManager* mInputMgr;
+  OIS::Keyboard* mKeyboard;
+  OIS::Mouse* mMouse;
+ 
+};
+ 
+#endif
